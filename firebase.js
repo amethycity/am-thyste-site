2cb1bf6808d9e301f1acce4a8efc8be07eca6f0d
amethycity <amethycity@gmail.com>
2026-05-28 21:33:23 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+const profileBox =
+document.getElementById("profile-box");
+
+if(profileBox){
+
+profileBox.innerHTML = `
+
+<a href="login.html"
+class="login-link">
+
+CONNEXION
+
+</a>
+
+`;
+
+}
